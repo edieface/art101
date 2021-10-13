90bf14c2b55b61b98f7d7671997b250346597607
edieface <91556538+edieface@users.noreply.github.com>
2021-10-13 12:19:34 -0700

    Lab 4
    I had a challenge with the picture uploads again, but my partners and I worked together on coming up with pseudocode and coding tasks.

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+//How to Roll Computer Generated Dice.
+//Coding a computer to roll die.
+//@link https://en.wikipedia.org/wiki/List_of_dice_games
+//@file This files defines the MyClass class.
+//@author Edie Trautwein.
+//@since 10/13/2021
+//Rolling Dice:
+  // Print numbers 1-6 (1,2,3,4,5,6)
+  // Accept user input
+  // Generate number (1,2,3,4,5,6)
+  // Print number
