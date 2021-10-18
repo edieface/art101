264c82b148c496cc34eff8be76124e4390ced9e3
edieface <91556538+edieface@users.noreply.github.com>
2021-10-18 16:42:24 -0700

    Lab 5: Data Types and Variables
    I had a challenge with the photos and also my script output. I tried going over the code and inspecting it but I was unsuccessful.

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+* Author: Edie Trautwein
+* Created: 18 October
+* License: Public Domain
+*/
+
+//Define Variables
+make = "Prius";
+model = "L";
+color = "Silver";
+year = "2012";
+
+//calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeLn("Year: " + year + "<br>");
+document.writeln("Age:" + age + " years<br>");
