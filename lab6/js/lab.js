34f3087f4c5cb6f948c8cafe463cebf299a3182f
edieface <91556538+edieface@users.noreply.github.com>
2021-10-20 10:15:09 -0700

    Lab 6
    In this lab we focussed on arrays and objects and I was successfully able to upload my image!!

---
+++ lab6/js/lab.js
@@ -0,0 +1,18 @@
+/**
+* Author: Edie Trautwein
+* Created: 20 October
+* License: Public Domain
+*/
+
+var myTransport = ["bike" , "walking" , "bus" , "carpool"];
+
+var myMainRide = {
+  myMainRide.name: "bus";
+  myMainRide.color: "blue";
+  myMainRide.size;"double wide"
+};
+
+//output
+document.writeln("Getting around: " + myTransport + "<br>");
+document.writeln ("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
