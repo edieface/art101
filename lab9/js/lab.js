9fbfa62f6f7b14258f896138bdac9a2400857a60
edieface <91556538+edieface@users.noreply.github.com>
2021-11-01 09:42:41 -0700

    Lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,16 @@
+/*
+* Author: Edie Trautwein
+* Created: 2 November
+* License: Public Domain
+*/
+
+getElementById()
+var=outputEI
+
+document.createElement("p")=new1EI
+
+//test function
+getElementByNum()
+var=new2EI
+
+//output
