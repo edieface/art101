9fdd92f929ca1dab9cd6969b9f0954815dadf583
edieface <91556538+edieface@users.noreply.github.com>
2021-12-04 11:55:21 -0800

    Final Labs

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,20 +1,18 @@
 /*
 * Author: Edie Trautwein
-* Created: 15 November
+* Created: 2 December
 * License: Public Domain
 */
 
-
-//Using the core $.ajax() method
 $.ajax({
   url: "https://yourapiendpoint.com/",
   data: {
-    id: 123
-    api_key: "blahblahblah",
+    id: 123,
+    api_key: "blahblahblah"
   },
   type: "GET",
-  dataType : "json",
-  success: function(data){
+  datatype: "json",
+  success: function(data) {
     console.log(data);
   },
   error: function (jqXHR, textStatus, errorThrown) {
